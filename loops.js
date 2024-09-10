74c3b72e7069b2246b938280a2c46663549c81ae
Erica The Developer <135877885+ericaverse@users.noreply.github.com>
2024-09-10 16:24:28 -0400

    Using Ternary for Pt. 2

--- loops.js
+++ loops.js
@@ -5,6 +5,7 @@
 /// If a number is divisible by 5, log “Buzz.”
 /// If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 /// If a number is not divisible by either 3 or 5, log the number.
+console.log(`---Part One: FIZZ BUZZ---`);
 let num;
 for (let i = 1; i < 101; i++) {
   num = 0 + i;
@@ -20,7 +21,21 @@ for (let i = 1; i < 101; i++) {
     console.log(num);
   }
 }
-
+console.log(`-----------------------`);
+console.log(`-----------------------`);
+console.log(`---Part Two: PRIME TIME---`);
 //Part Two: Prime Time
-
+let one = 1;
+let n = 0;
+let prime = false;
+//Finding Prime Number
+for (let i = 1; i < 21; i++) {
+  n += i;
+  n == 1 || n == 2 || n == 3 || n % 6 == 1
+    ? console.log(!prime + `, ` + n + `is prime.`)
+    : console.log(prime + `,` + n + ` is not prime.`);
+}
+console.log(`-----------------------`);
+console.log(`-----------------------`);
+console.log(`---Part Three: Feeling Loopy---`);
 //Part Three: Feeling Loopy
